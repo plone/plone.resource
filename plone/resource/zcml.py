dff3379187f54e941ee041c215358bcf1024976d
Karl Johan Kleist <karl.johan@kleist-it-consulting.de>
2011-10-20 18:04:44 +0200

    Use os.path.isabs() to test if absolute path, this works on Windows too. Closes #12271

--- plone/resource/zcml.py
+++ plone/resource/zcml.py
@@ -1,4 +1,5 @@
 import os.path
+
 from zope.interface import Interface
 from zope.component.zcml import handler
 from zope.configuration.exceptions import ConfigurationError
@@ -21,8 +22,8 @@ class IResourceDirectoryDirective(Interface):
 
     name = PythonIdentifier(
         title = u'Name',
-        description = u'Name of the directory. If not specified, the name of the '
-                      u' current package is used.',
+        description = u'Name of the directory. If not specified, the name of '
+                      u'the current package is used.',
         required = False,
         )
 
@@ -37,28 +38,30 @@ class IResourceDirectoryDirective(Interface):
 def registerResourceDirectory(_context, directory, name=None, type=None):
     """
     Register a new resource directory.
-    
+
     The actual ZCA registrations are deferred so that conflicts can be resolved
     via zope.configuration's discriminator machinery.
     """
-    
-    if _context.package and directory.startswith('/'):
+
+    if _context.package and os.path.isabs(directory):
         raise ConfigurationError('Resource directories in distributions must '
                                  'be specified as relative paths.')
     elif _context.package:
         directory = _context.path(directory)
-    elif not _context.package and not directory.startswith('/'):
+    elif not _context.package and not os.path.isabs(directory):
         raise ConfigurationError('Global resource directories must be '
                                  'specified as absolute paths.')
 
+    # TODO: make sure this works in Windows
     if '..' in directory.split('/'):
-        raise ConfigurationError('Traversing to parent directories via .. is not allowed.')
+        raise ConfigurationError('Traversing to parent directories '
+                                 'via .. is not allowed.')
     if not os.path.exists(directory):
         raise IOError, 'Directory not found: %s' % directory
-    
+
     if name is None and _context.package:
         name = _context.package.__name__
-    
+
     if type:
         identifier = '++%s++%s' % (type, name or '')
     else:
@@ -66,10 +69,10 @@ def registerResourceDirectory(_context, directory, name=None, type=None):
             raise ConfigurationError('Resource directories in distributions '
                                      'must have a specified resource type.')
         identifier = name or ''
-    
+
     directory = os.path.sep.join(directory.split('/'))
     directory = FilesystemResourceDirectory(directory, name)
-    
+
     _context.action(
         discriminator = ('plone:static', identifier),
         callable = handler,
