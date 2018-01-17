c03833af8f6180f16eef4a8172a1fa44150695f2
Maurits van Rees <maurits@vanrees.org>
2018-01-17 21:01:46 +0100

    Removed Plone 5.0 and 5.1 classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ setup(
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
