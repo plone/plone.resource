fd16c42f728b7be4cddf2c26f9ade245191b07ad
Maurits van Rees <maurits@vanrees.org>
2018-01-17 18:51:54 +0100

    Added GPL license classifier to setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
